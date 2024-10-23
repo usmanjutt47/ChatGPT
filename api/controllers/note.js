93b5cb1b235eab338e2df940822adf043eec3ecf
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-10-23 11:31:55 +0500

    Create note.js

---
+++ api/controllers/note.js
@@ -0,0 +1,35 @@
+const Note = require("../models/Note");
+
+const createNote = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const image = req.file ? req.file.path : null;
+
+    const newNote = new Note({
+      title,
+      description,
+      image,
+    });
+
+    await newNote.save();
+    res
+      .status(201)
+      .json({ message: "Note created successfully", note: newNote });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create note" });
+  }
+};
+
+const getNotes = async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to get notes" });
+  }
+};
+
+module.exports = {
+  createNote,
+  getNotes,
+};
