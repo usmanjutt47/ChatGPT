949bb5fa6782942b204cb596f9f46a6ef4bded96
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-10-23 11:32:15 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,46 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
 import Home from "./screens/Home";
 import Details from "./screens/Details";
 import CreateNotes from "./screens/CreateNotes";
 
 const Stack = createNativeStackNavigator();
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      PlusJakartaSansMedium: require("./assets/fonts/PlusJakartaSans-Medium.ttf"),
+      PlusJakartaSansBold: require("./assets/fonts/PlusJakartaSans-Bold.ttf"),
+    });
+  };
+
+  useEffect(() => {
+    const loadResources = async () => {
+      try {
+        await loadFonts();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setFontsLoaded(true);
+        await SplashScreen.hideAsync();
+      }
+    };
+
+    loadResources();
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -23,12 +54,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
