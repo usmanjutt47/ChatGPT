165a8f72bdcb1202a0698088379b520866d11f4d
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-10-26 09:50:18 +0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const colors = require("colors");
 const connectDB = require("./config/db");
 const noteRoutes = require("./routes/noteRoutes");
@@ -10,8 +9,6 @@ const port = 3000;
 connectDB();
 
 app.use(express.json());
-
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api", noteRoutes);
 
