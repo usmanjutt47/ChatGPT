71ff88bc1dd69c7877f5cb91dd67f4b8f6a78388
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-10-26 09:50:15 +0500

    Update noteRoutes.js

--- api/routes/noteRoutes.js
+++ api/routes/noteRoutes.js
@@ -1,21 +1,9 @@
 const express = require("express");
-const multer = require("multer");
 const { createNote, getNotes } = require("../controllers/note");
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({ storage: storage });
-
-router.post("/notes", upload.single("image"), createNote);
+router.post("/notes", createNote);
 router.get("/notes", getNotes);
 
 module.exports = router;
