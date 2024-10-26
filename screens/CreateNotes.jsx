c7d76325ae6ee0cb7d9d23820a660769b7ba288c
Muhammad Usman Tariq <142773789+usmanjutt47@users.noreply.github.com>
2024-10-26 09:50:36 +0500

    Update CreateNotes.jsx

--- screens/CreateNotes.jsx
+++ screens/CreateNotes.jsx
@@ -1,178 +1,214 @@
 import {
-  View,
+  ScrollView,
   StyleSheet,
-  TouchableOpacity,
   TextInput,
+  TouchableHighlight,
+  View,
+  Modal,
   Text,
-  Pressable,
-  ImageBackground,
 } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import AntDesign from "@expo/vector-icons/AntDesign";
 import { useNavigation } from "@react-navigation/native";
 import Entypo from "@expo/vector-icons/Entypo";
-import * as ImagePicker from "expo-image-picker";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import axios from "axios";
 
 export default function CreateNotes() {
   const navigation = useNavigation();
-  const [image, setImage] = useState(null);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
 
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
+  const handleBack = () => {
+    navigation.goBack();
+  };
 
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
+  const handleSave = () => {
+    setModalVisible(true);
+  };
+
+  const confirmSave = async () => {
+    try {
+      const response = await axios.post("http://192.168.10.10:3000/api/notes", {
+        title,
+        description,
+      });
+      navigation.goBack();
+    } catch (error) {
+      console.error("Error creating note:", error);
+    } finally {
+      setModalVisible(false);
     }
   };
 
-  const deleteImage = () => {
-    setImage(null);
-  };
-
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#f4f4f4" }}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.innerContainer}>
-        <TouchableOpacity
-          style={styles.backContainer}
-          onPress={() => navigation.goBack()}
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
         >
-          <AntDesign name="arrowleft" size={24} color="#f4f4f4" />
-        </TouchableOpacity>
-
-        <View style={styles.inputsContainer}>
-          <Text style={styles.title}>Title</Text>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <TouchableHighlight
+              style={styles.addContainer}
+              onPress={handleBack}
+            >
+              <Entypo name="chevron-left" size={30} color="#fff" />
+            </TouchableHighlight>
+            <TouchableHighlight
+              style={styles.addContainer}
+              onPress={handleSave}
+            >
+              <MaterialIcons name="save" size={24} color="#fff" />
+            </TouchableHighlight>
+          </View>
           <TextInput
-            placeholder="Add title"
-            style={styles.input}
-            cursorColor={"#272731"}
+            placeholder="Title"
+            style={styles.titleInput}
+            cursorColor={"#9A9A9A"}
+            placeholderTextColor={"#9a9a9a"}
             multiline={true}
             value={title}
-            onChangeText={(text) => setTitle(text)}
+            onChangeText={setTitle}
           />
-          <Text style={styles.dec}>Description</Text>
           <TextInput
-            placeholder="Add your description"
-            style={styles.descInput}
-            cursorColor={"#272731"}
+            placeholder="Type something..."
+            style={styles.titleDesc}
+            cursorColor={"#9A9A9A"}
+            placeholderTextColor={"#9a9a9a"}
             multiline={true}
             value={description}
-            onChangeText={(text) => setDescription(text)}
+            onChangeText={setDescription}
           />
-
-          <TouchableOpacity style={styles.uploadPicture} onPress={pickImage}>
-            {!image ? (
-              <Entypo name="upload-to-cloud" size={30} color="black" />
-            ) : (
-              <ImageBackground source={{ uri: image }} style={styles.image}>
-                <Pressable style={styles.deleteButton} onPress={deleteImage}>
-                  <MaterialIcons name="delete" size={20} color="#fff" />
-                </Pressable>
-              </ImageBackground>
-            )}
-          </TouchableOpacity>
-        </View>
-
-        <TouchableOpacity style={styles.btn}>
-          <Text style={{ color: "#fff" }}>Add</Text>
-        </TouchableOpacity>
+        </ScrollView>
       </View>
+      <Modal
+        transparent={true}
+        animationType="fade"
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContainer}>
+            <View
+              style={{
+                height: "90%",
+                width: "90%",
+                alignSelf: "center",
+                justifyContent: "center",
+                alignContent: "center",
+              }}
+            >
+              <View style={{ alignSelf: "center" }}>
+                <MaterialIcons name="info" size={30} color="#606060" />
+              </View>
+              <Text style={styles.modalMessage}>Save changes ?</Text>
+              <View style={styles.modalButtons}>
+                <TouchableHighlight
+                  onPress={() => setModalVisible(false)}
+                  style={styles.button}
+                >
+                  <Text style={styles.buttonText}>Discard</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                  onPress={confirmSave}
+                  style={styles.saveButton}
+                >
+                  <Text style={styles.buttonText}>Save</Text>
+                </TouchableHighlight>
+              </View>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  innerContainer: {
+  container: {
+    backgroundColor: "#252525",
     flex: 1,
+  },
+  innerContainer: {
+    height: "100%",
     width: "95%",
     alignSelf: "center",
-    justifyContent: "space-between",
   },
-  backContainer: {
-    width: 48,
-    height: 48,
-    backgroundColor: "#272731",
-    borderRadius: 12,
+  addContainer: {
+    height: 50,
+    width: 50,
+    backgroundColor: "#3B3B3B",
+    borderRadius: 15,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 20,
   },
-  inputsContainer: {
+  titleInput: {
+    fontSize: 40,
+    color: "#9A9A9A",
+    fontFamily: "PlusJakartaSansSemiBold",
+    marginTop: 30,
+  },
+  titleDesc: {
+    fontSize: 23,
+    color: "#9A9A9A",
+    fontFamily: "PlusJakartaSansRegular",
+    marginTop: 30,
+  },
+  modalOverlay: {
     flex: 1,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginTop: 20,
-  },
-  input: {
-    height: 48,
-    width: "100%",
-    borderRadius: 12,
-    borderWidth: 1,
-    borderColor: "#272731",
-    paddingLeft: 10,
-    paddingRight: 10,
-    marginTop: 5,
-  },
-  descInput: {
-    height: 48,
-    width: "100%",
-    borderRadius: 12,
-    borderWidth: 1,
-    borderColor: "#272731",
-    paddingLeft: 10,
-    paddingRight: 10,
-    marginTop: 5,
-  },
-  dec: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginTop: 10,
-  },
-  uploadPicture: {
-    width: "100%",
-    height: 150,
-    borderWidth: 1,
-    borderRadius: 12,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 20,
+    backgroundColor: "rgba(4000, 4000, 4000, 0.1)",
   },
-  image: {
-    height: "100%",
+  modalContainer: {
+    width: "85%",
+    height: "25%",
+    backgroundColor: "#252525",
+    borderRadius: 20,
+    alignItems: "center",
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  modalMessage: {
+    fontSize: 16,
+    textAlign: "center",
+    marginVertical: 10,
+    color: "#CFCFCF",
+  },
+  modalButtons: {
+    flexDirection: "row",
     width: "100%",
-    overflow: "hidden",
-    borderRadius: 12,
-    zIndex: 9999,
+    justifyContent: "center",
+    gap: 10,
   },
-  deleteButton: {
+  button: {
     backgroundColor: "#ff0000",
-    height: 35,
-    width: 35,
-    borderRadius: 99,
+    borderRadius: 5,
+    height: 39,
+    width: 112,
     justifyContent: "center",
     alignItems: "center",
-    position: "absolute",
-    right: 5,
-    bottom: 5,
-    zIndex: 99,
+    marginTop: 20,
   },
-  btn: {
-    width: "100%",
-    height: 48,
-    backgroundColor: "#272731",
-    borderRadius: 12,
+  saveButton: {
+    backgroundColor: "#30BE71",
+    borderRadius: 5,
+    height: 39,
+    width: 112,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: "5%",
+    marginTop: 20,
+  },
+
+  buttonText: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
   },
 });
